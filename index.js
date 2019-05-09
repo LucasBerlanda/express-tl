2c87e92dd1c572f360d1607209232c74efa5ab51
Lucas Berlanda <lucasberlanda02@gmail.com>
2019-05-09 01:36:57 -0300

    feito funcionar cadastros de ufs e listagem

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const tl = require('express-tl');
 const bodyParser = require("body-parser");// pegar parametros do request
 
 const uf = require('./controllers/UnidadeFederativaController');
-const cidade = require('./models/UnidadeFederativa');
+const cidade = require('./controllers/CidadeController');
+//const cidade = require('./models/UnidadeFederativa');
 const server = express();
 
 server.use(bodyParser.urlencoded({ extended: true }));
@@ -16,5 +17,9 @@ server.set('view engine', 'tl') // register the template engine
 server.get('/uf/cadastro', uf.cadastro);
 server.get('/uf/lista', uf.lista);
 server.post('/uf/salvar', uf.salvar);
+
+server.get('/cidade/cadastro', cidade.cadastro);
+server.get('/cidade/lista', cidade.lista);
+server.post('/cidade/salvar', cidade.salvar);
 
 server.listen(3000);
